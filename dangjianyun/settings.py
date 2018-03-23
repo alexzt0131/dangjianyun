6dd6058d335749b9b55fc6720ba9aad68b2772b8
alexzt0131 <110021341@qq.com>
2018-03-23 21:25:15 +0800

    Update settings.py

--- dangjianyun/settings.py
+++ dangjianyun/settings.py
@@ -128,12 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
+STATIC_ROOT = BASE_DIR + '/static' 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/root/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
+# ]
 
 
 
