63c8faf496d1428b0b8fa90f6d3a8127719fd2cb
alexzt0131 <110021341@qq.com>
2018-03-23 20:20:56 +0800

    Update settings.py

--- dangjianyun/settings.py
+++ dangjianyun/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = '/root/www/dangjianyun'
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
